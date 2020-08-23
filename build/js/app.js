a137b9c8bfae1a22371040cf1b79deef65f65934
julymole <64532834+julymole@users.noreply.github.com>
2020-08-24 01:17:22 +0300

    правки

--- build/js/app.js
+++ build/js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const details = document.querySelector('details')
+/* var details = document.querySelector('details');
 
-details.open = true  // Отобразить содержимое
-details.open = false // Скрыть содержимое
+details.open = true;  // Отобразить содержимое
+details.open = false; // Скрыть содержимое
 // Наблюдатель, который будет следить за изменением атрибута open
-const observer = new MutationObserver(function(mutations) {
+var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
 
     // Елемент у которого измелился атрибут
-    const details = mutation.target
+    var details = mutation.target
 
     // Если елемент был закрыт — ничего не делаем
     if (!details.open) {
@@ -39,4 +39,4 @@ const config = {
 
 
 // Вешаем наблюдатель на все елементы details внутри акордиона
-document.querySelectorAll('details').forEach(el => observer.observe(el, config))
+document.querySelectorAll('details').forEach(el => observer.observe(el, config)) */
