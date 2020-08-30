7acb452bf8c7b3929ad8e715df41eaa2043fed5d
julymole <64532834+julymole@users.noreply.github.com>
2020-08-31 00:24:56 +0300

    jquery: маска для input tel, открытие и закрытие модальных окон, переключение текстовых блоков в секции "программы"

--- build/js/app.js
+++ build/js/app.js
@@ -42,9 +42,66 @@ const config = {
 document.querySelectorAll('details').forEach(el => observer.observe(el, config)) */
 
 
-const modal = document.querySelector(".popup");
+/* const modal = document.querySelector(".popup");
 const btnClose = document.querySelector(".popup__close");
 
 btnClose.addEventListener("click", function (){
     modal.classList.add("popup");
+}); */
+
+$(document).ready(function () {
+  $("#phone").mask("+7 (999) 999-99-99");
+
+  // Открытие popup при клике на "заказать звонок"
+  $('.header__row-toform').click(function () {
+    $('.popup').fadeIn();
+  });
+
+  // Закрытие popup при клике на крестик
+  $('.popup__close').click(function () {
+    $('.popup').fadeOut();
+  });
+
+  // Открытие popup "заявка принята" при клике на кнопки "перезвоните мне"
+  $('input[value*="перезвоните"]').click(function () {
+    var errors = 0;
+    var fields = $('input[type=text], input[type=tel]')
+    fields.each(function () {
+      var value = $(this).val();
+      if (value.length < 3 || value == 'имя' || value == 'телефон') {
+        $(this).css('background', '#f4e2e2');
+        errors++;
+      } else {
+        $(this).css('background', 'none');
+      }
+    });
+    if (!errors) {
+      $('.popup-taken').fadeIn();
+    }
+  });
+
+  // Закрытие popup "заявка принята" при клике на крестик
+  $('.popup-taken__close').click(function () {
+    $('.popup-taken').fadeOut();
+  });
+  $('.programs__buttons-wrapper-btn:nth-of-type(1)').click(function () {
+    $('.block').css('display', 'none');
+    $('.block:nth-of-type(2)').css('display', 'block');
+  });
+  $('.programs__buttons-wrapper-btn:nth-of-type(2)').click(function () {
+    $('.block').css('display', 'none');
+    $('.block:nth-of-type(3)').css('display', 'block');
+  });
+  $('.programs__buttons-wrapper-btn:nth-of-type(3)').click(function () {
+    $('.block').css('display', 'none');
+    $('.block:nth-of-type(4)').css('display', 'block');
+  });
+  $('.programs__buttons-wrapper-btn:nth-of-type(4)').click(function () {
+    $('.block').css('display', 'none');
+    $('.block:nth-of-type(5)').css('display', 'block');
+  });
+  $('.programs__buttons-wrapper-btn:nth-of-type(5)').click(function () {
+    $('.block').css('display', 'none');
+    $('.block:nth-of-type(6)').css('display', 'block');
+  });
 });
